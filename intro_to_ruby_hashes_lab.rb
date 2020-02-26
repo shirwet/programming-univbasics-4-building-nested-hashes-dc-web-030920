09f22fed789dcc1477c442810e9c909d99ed478c
shirwet Elfeel <eshirwet@yahoo.com>
2020-02-26 13:14:13 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -13,14 +13,11 @@ def monopoly_with_second_tier
   # they'll guide you in how to modify what you just did to have more
   # complexity.
   
- 	monopoly = {
+ monopoly = {
  	  :railroads => {
- 	    :pieces =>4,
- 	    :rent_in_dollars => {
- 	    
- 	    }
- 	    }
- 	    },
+ 	    :pieces =>4
+ 	  }
+ 	}
  	  
  	  end
 
