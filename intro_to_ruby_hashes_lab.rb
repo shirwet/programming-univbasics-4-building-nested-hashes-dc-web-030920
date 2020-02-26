3342810fe579a8408d0b09f3941f7a1c479fa62a
shirwet Elfeel <eshirwet@yahoo.com>
2020-02-26 13:13:07 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -17,10 +17,7 @@ def monopoly_with_second_tier
  	  :railroads => {
  	    :pieces =>4,
  	    :rent_in_dollars => {
- 	      :one_piece_owned =>25,
-         :two_pieces_owned =>50,
-         :three_pieces_owned =>100,
-         :four_pieces_owned =>200
+ 	    
  	    }
  	    }
  	    },
@@ -34,11 +31,19 @@ def monopoly_with_third_tier
   # complexity.
   
    monopoly = {
- 	  :railroads => { :pieces => 4, :rent_in_dollars
- 	  }
- 	}
+ 	 monopoly = {
+ 	  :railroads => {
+ 	    :pieces =>4,
+ 	    :rent_in_dollars => {
+ 	      :one_piece_owned =>25,
+         :two_pieces_owned =>50,
+         :three_pieces_owned =>100,
+         :four_pieces_owned =>200
+ 	    }
+ 	    }
+ 	    },
 end
-end
+
 
 def monopoly_with_fourth_tier
   # When you start writing the implementation for this method, copy the Hash
