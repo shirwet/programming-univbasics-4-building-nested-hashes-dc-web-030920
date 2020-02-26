f8526ecef71c0ad14f6af02844c404cae8c6b76e
shirwet Elfeel <eshirwet@yahoo.com>
2020-02-26 13:27:11 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -65,7 +65,7 @@ def monopoly_with_fourth_tier
  	    :names => {
  	      :reading_railroad => {"mortgage_value" =>"$100"},
  	      :b_and_o_railroad => {"mortgage_value" =>"$400"},
- 	      :shortline_railroad => {},
+ 	      :shortline_railroad => {"mortgage_value" =>"$800"},
  	      :pennsylvania_railroad => {"mortgage_value" =>"$200"}
  	    }
  	    }
