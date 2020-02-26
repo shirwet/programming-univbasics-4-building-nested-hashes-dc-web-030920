2d99b0569bb370a57811c87569696ca486bdd427
shirwet Elfeel <eshirwet@yahoo.com>
2020-02-26 13:12:02 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -21,7 +21,10 @@ def monopoly_with_second_tier
          :two_pieces_owned =>50,
          :three_pieces_owned =>100,
          :four_pieces_owned =>200
+ 	    }
+ 	    }
  	    },
+ 	  
  	  end
 
 def monopoly_with_third_tier
